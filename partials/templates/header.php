ac3f566debe695fe167edd5a5243d58c92e2c6de
davide sbalzer <sbalzerdavide@hotmail.it>
2020-12-17 21:16:17 +0100

    import fontawesome

--- partials/templates/header.php
+++ partials/templates/header.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="img">
             <img src="./image/logo.png" alt="">
+            ciao <i class="fas fa-server"></i>
         </div>
     </div>
 </header>
